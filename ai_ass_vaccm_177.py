d6d02bd5399b94c04f7d53eef273a3f8c527e6f4
Aniket Sawant <67280854+dev77492@users.noreply.github.com>
2020-07-17 22:13:53 +0530

    Add files via upload

---
+++ ai_ass_vaccm_177.py
@@ -0,0 +1,18 @@
+" program to implement vacuum cleaner"
+
+a=int(input("Enter the number of rooms :"));
+"print (a);"
+
+for i in range (a):
+     room1=input("Enter room name :");
+     r1=int(input("Is garbage present in that room ?press 1 if present else press 0 :"));
+     if(r1==1):
+          print("The cleaner is sucking.....");
+          print("This room is clean moving to the next one ....");
+     else :
+          print("Moving to the next room....");
+
+
+print("Your house is clean now... Enjoy your time . Thank you !!");
+     
+
